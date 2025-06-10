e0093ac39fb71301499215752d345a9c7562d9bd
WolfOP <38270293+WolfOP@users.noreply.github.com>
2025-06-10 16:20:19 +1000

    Update KeySkillsHub.js
    added key skills

--- components/KeySkillsHub.js
+++ components/KeySkillsHub.js
@@ -1,16 +1,30 @@
-// components/KeySkillsHubFull.js
-// Previously this module exported a React component written in JSX. Since
-// the project does not include a build step to transform JSX, importing that
-// component caused a syntax error in the browser. To avoid runtime errors we
-// now export a simple function that returns HTML as a string.  Interactive
-// versions of the Key Skills activities can be reinstated once a build
-// process is added.
+import KeySkill1 from "./keyskillscomponets/KeySkill1.js";
+import KeySkill2 from "./keyskillscomponets/KeySkill2.js";
+import KeySkill3 from "./keyskillscomponets/KeySkill3.js";
+import KeySkill4 from "./keyskillscomponets/KeySkill4.js";
+import KeySkill5 from "./keyskillscomponets/KeySkill5.js";
+import KeySkill6 from "./keyskillscomponets/KeySkill6.js";
+import KeySkill7 from "./keyskillscomponets/KeySkill7.js";
+import KeySkill8 from "./keyskillscomponets/KeySkill8.js";
+import KeySkill9 from "./keyskillscomponets/KeySkill9.js";
+import KeySkill10 from "./keyskillscomponets/KeySkill10.js";
 
 export default function KeySkillsHub() {
   return `
     <section class="p-8 text-white">
       <h1 class="text-3xl font-bold mb-6 text-purple-400">Unit 3 SAC 2 – Key Skills Hub</h1>
-      <p class="text-slate-300">Interactive Key Skills activities will be available soon.</p>
+      <p class="text-slate-300 mb-4">Interactive Key Skills activities:</p>
+
+      ${KeySkill1()}
+      ${KeySkill2()}
+      ${KeySkill3()}
+      ${KeySkill4()}
+      ${KeySkill5()}
+      ${KeySkill6()}
+      ${KeySkill7()}
+      ${KeySkill8()}
+      ${KeySkill9()}
+      ${KeySkill10()}
     </section>
   `;
 }
